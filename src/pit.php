1a78048c09d8d0885e1da5571552270b7384ec0e
Chris Keavey <chris@chriskeavey.com>
2015-11-30 17:43:23 +0000

    Presentation changes

--- src/pit.php
+++ src/pit.php
@@ -21,7 +21,7 @@
   <section class="grey" id="about">
     <div class="row content">
       <div class="small-12 columns">
-        <h1>Modular Pre-Cast Concrete Inspection Pit</h1>
+        <h3>Modular Pre-Cast Concrete Inspection Pit</h3>
         <p>Cambrian Transport have developed a system of modular, pre-cast concrete inspection pit units. Their first installation was designed to provide a semi-permanent inspection and maintenance pit facility for a track renewals contractor who required a depot with just a two-year operational life. A tight project deadline meant that the pit had to be constructed in as short a time as practicable.</p>
 
         <p>The U-shaped concrete pit units, designed and refined by Cambrianco in conjunction with ABM, had to be cast to fine tolerances. Rail base plates are bolted directly to the units. Steel shims and eccentric ferrules are used to fine-line the track if needed.</p>
@@ -31,18 +31,10 @@
     </div>
   </section>
 
-  <section>
-    <div class="banner splash">
-      <div class="row">
-        <p class="quote">We aim to be the first choice for Transport Consultancy services.</p>
-      </div>
-    </div>
-  </section>
-
   <section class="grey" id="about">
     <div class="row content">
       <div class="small-12 columns">
-        <h1>Rapid Comissioning</h1>
+        <h3>Rapid Comissioning</h3>
         <p>A complete inspection pit can be constructed in a week, minimising disruption during construction and ensuring the rapid commissioning of the new facility. The pit is tanked to prevent problems with ground water and contain any spillages of fuel, oil or cleaning chemicals from the rail vehicle maintenance operation. A pumped drain with an interceptor is provided to collect and spillages and to allow safe environmental disposal. Pit lighting and power supplies can be provided to meet the operator's requirements.</p>
       </div>
     </div>
@@ -51,7 +43,7 @@
     <section class="grey" id="about">
       <div class="row content">
         <div class="small-12 columns">
-          <h1>Relocatable and Reusable</h1>
+          <h3>Relocatable and Reusable</h3>
           <p>The pit is designed to be relocatable and reusable. This can assist an operator's business investment case. It also minimises the environment impact of the project. The system is suitable for either temporary or permanent depots. The units themselves have a 30-year design life.</p>
 
           <p>The modular pit solution has been further refined and developed in other applications to form the basis of a complete rail vehicle maintenance depot. Side pit units with removable steel covers are available. These further extend the capability of the depot facility by allowing pit access to the outside of vehicle bogies as well as to the underside. Pre-cast slab track areas for maintenance, cleaning or fuelling away from the inspection pit are available.</p>
